7380af428391ad6ecbbfb2f9ec51dfb7a8964539
Ezequiel Alejandro Zapata <ezequielalejandrozapata@gmail.com>
2024-07-04 01:09:42 -0300

    entrega final 2

--- src/componentes/Main/Carrito.jsx
+++ src/componentes/Main/Carrito.jsx
@@ -7,20 +7,26 @@ function Carrito() {
     const {carrito, calcularTotal, borrarProducto,vaciarCarrito} = useContext(CartContext);
 
     return (
-    <div>
+    <div className='carrito'>
         {carrito.map((producto =>  
         <Fragment key={producto.id}>
-        <div> 
-        <h1> {producto.nombre}:${producto.precio}</h1>
-        <button onClick={()=> borrarProducto(producto.id)}>borrar x</button>
-        <p>cantidad:{producto.cantidad}</p>
+        <div className='contenedor-carrito'> 
+            <img className="producto-imagen" src={producto.imagen} alt={producto.nombre} />
+            <div className='contenedor-npc'>
+            <h2 className='nombre-y-precio'> {producto.nombre}: ${producto.precio}</h2>
+            <p>cantidad:{producto.cantidad}</p>
+            </div>
+            <button className='boton-borrar' onClick={()=> borrarProducto(producto.id)}>x</button>
         </div>
         </Fragment>))}
         {carrito.length > 0 ? 
         <>
-        <h2>Total:: ${calcularTotal()}</h2>
-        <button onClick={vaciarCarrito}>vaciar carrito</button>
-        <Link to="/checkout">Finalizar Compra</Link>
+        <div className='carrito-total'>
+        <h2>Total: ${calcularTotal()}</h2>
+        <button className='vaciar-carrito' onClick={vaciarCarrito}>limpiar</button>
+        </div>
+        <Link className='finalizar-compra' to="/checkout">Finalizar Compra</Link>
+        
         </> : <h2 className='carrito-vacio'> Carrito vacio...</h2> }
     </div>
     )
--- src/componentes/Main/Checkout.jsx
+++ src/componentes/Main/Checkout.jsx
@@ -37,22 +37,24 @@ function Checkout() {2
     }
     return (
         <div>
-            <form onSubmit={handleSubmit(comprar)}>
-                <div>
-                    <p>este es su pedido</p>
+            <form className='formulario' onSubmit={handleSubmit(comprar)}>
+                <h1>Este es su pedido</h1>
+                <div className='producto-card-check'>
                     {carrito.map((prod)=>{
-                        return<>
-                        <img className="producto-imagen" src={prod.imagen} alt={prod.nombre} />
-                        <h2 className='producto-titulo'>{prod.nombre}</h2>
-                        <p className='producto-precio'>${prod.precio}</p>
-                        <p>{prod.cantidad}</p>
-                        </>
+                        return  <div className='contenedor-info-check'>
+                                    <img className="producto-imagen" src={prod.imagen} alt={prod.nombre} />
+                                    <h2 className='producto-titulo-check'>{prod.nombre}</h2>
+                                    <p className='producto-precio-check'>${prod.precio}</p>
+                                    <p>cantidad:{prod.cantidad}</p>
+                                </div>
                     })}
-                    <p>este es el total:{calcularTotal()}</p>
                 </div>
+                <h2 className='total-compra'>Este es el total: ${calcularTotal()}</h2>
+                <div className='datos-usuarios'>
                 <input type="text" placeholder="ingrese su nombre y apellido" {...register("nombre")} />
                 <input type="email" placeholder='ingrese su e-mail' {...register("email")}/>
                 <button type="submit">comprar</button>
+                </div>
             </form>
         </div>
     )
